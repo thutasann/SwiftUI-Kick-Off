32bfcdc8fa65a0792b5f180516143570a647d980
thutasann <thutasann@gmail.com>
2022-11-29 14:20:20 +0630

    Initial Commit

---
+++ SwiftUI-KickOff/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-KickOff
+//
+//  Created by Thuta sann on 11/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-KickOff/SwiftUI_KickOffApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_KickOffApp.swift
+//  SwiftUI-KickOff
+//
+//  Created by Thuta sann on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_KickOffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
